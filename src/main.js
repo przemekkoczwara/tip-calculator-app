e9c1e03339c081c902ed0fb9013a96aff4b50af0
Przemylaw Koczwara <134004284+przemekkoczwara@users.noreply.github.com>
2025-11-29 20:24:00 +0100

    Add logical js to main.js -> calculator tip function

--- src/main.js
+++ src/main.js
@@ -30,28 +30,85 @@ tipButtons.forEach((button) => {
     customTipInput.classList.add('hidden');
     customTipInput.value = '';
 
-    console.log(`Selected tip: ${tipPercentage}%`);
+    // console.log(`Selected tip: ${tipPercentage}%`);
 
-    // calculateTip();
-  });
-
-  // custom button
-
-  customButton.addEventListener('click', () => {
-    tipButtons.forEach((btn) => btn.classList.remove('active'));
-    customButton.classList.add('active');
-
-    customTipInput.classList.remove('hidden');
-    customTipInput.focus();
-
-    console.log('Custom btn active');
-  });
-
-  // custom tip input
-
-  customTipInput.addEventListener('input', (e) => {
-    tipPercentage = parseFloat(e.target.value) || 0;
-
-    console.log(`Selected tip: ${tipPercentage}%`);
+    calcuateTip();
   });
 });
+
+// custom button
+
+customButton.addEventListener('click', () => {
+  tipButtons.forEach((btn) => btn.classList.remove('active'));
+  customButton.classList.add('active');
+
+  customTipInput.classList.remove('hidden');
+  customTipInput.focus();
+
+  // console.log('Custom btn active');
+});
+
+// custom tip input
+
+customTipInput.addEventListener('input', (e) => {
+  tipPercentage = parseFloat(e.target.value) || 0;
+  calcuateTip();
+  // console.log(`Selected tip: ${tipPercentage}%`);
+});
+
+// bill input
+
+billInput.addEventListener('input', (e) => {
+  billValue = parseFloat(e.target.value) || 0;
+  calcuateTip();
+  //   console.log(`Introduce bill: ${billValue}`);
+});
+
+// people input
+
+peopleInput.addEventListener('input', (e) => {
+  numberOfPeople = parseFloat(e.target.value) || 0;
+
+  //   console.log(`Number: ${numberOfPeople}`);
+
+  if (numberOfPeople === 0) {
+    errorMessage.classList.remove('hidden');
+    peopleInput.classList.add('error-msg');
+  } else {
+    errorMessage.classList.add('hidden');
+    peopleInput.classList.remove('error-msg');
+  }
+  calcuateTip();
+});
+
+resetButton.addEventListener('click', (e) => {
+  billValue = 0;
+  tipPercentage = 0;
+  numberOfPeople = 1;
+  billInput.value = '0';
+  customTipInput.value = '';
+  peopleInput.value = '0';
+
+  tipButtons.forEach((btn) => btn.classList.remove('active'));
+  customButton.classList.remove('active');
+  customTipInput.classList.add('hidden');
+  errorMessage.classList.add('hidden');
+  peopleInput.classList.remove('error-msg');
+  tipAmountDisplay.textContent = '$0.00';
+  tipTotalDisplay.textContent = '$0.00';
+});
+
+function calcuateTip() {
+  if (billValue > 0 && tipPercentage > 0 && numberOfPeople > 0) {
+    const tipAmount = (billValue * tipPercentage) / 100;
+
+    const tipPerPerson = tipAmount / numberOfPeople;
+    const totalPerson = (billValue + tipAmount) / numberOfPeople;
+
+    tipAmountDisplay.textContent = tipPerPerson.toFixed(2);
+    tipTotalDisplay.textContent = totalPerson.toFixed(2);
+    // console.log(`
+    // Tip per person: ${tipPerPerson},
+    // Total per person ${totalPerson}`);
+  }
+}
